e985540786f102e76c0de8cc38b1e20cda6856f3
Ragav1196 <ragavinrap@gmail.com>
2022-03-18 17:52:36 +0530

    ERROR

--- src/Components/Global Constants/GlobalConstants.js
+++ src/Components/Global Constants/GlobalConstants.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:9000";
-// export const API_URL = "https://my-splitwise-clone.herokuapp.com";
+// export const API_URL = "http://localhost:9000";
+export const API_URL = "https://my-splitwise-clone.herokuapp.com";
